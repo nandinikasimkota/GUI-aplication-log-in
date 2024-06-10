3ec645beb83682ba785e129e4e32498bb4fc92ac
nandinikasimkota <148858405+nandinikasimkota@users.noreply.github.com>
2024-06-10 09:35:36 +0530

    Add files via upload

---
+++ smallproject.py
@@ -0,0 +1,28 @@
+#GUI Application
+#log in
+from tkinter import *
+from tkinter import messagebox
+def ok():
+    uname = e1.get()
+    password = e2.get()
+    if (uname == "" and password == ""):
+        messagebox.showinfo("", "Blank Not allowed")
+    elif(uname == "Admin" and  password == "123"):
+        messagebox.showinfo("", "Login Success")
+        root.destroy()
+    else:
+        messagebox.showinfo("","Incorrect username and password")
+root = Tk()
+root.title("Login")
+root.geometry("300x200")
+global e1
+global e2
+Label(root, text="UserName").place(x=10, y=10)
+Label(root, text="password").place(x=10, y=40)
+e1 = Entry(root)
+e1.place(x=140, y=10)
+e2 = Entry(root)
+e2.place(x=140, y=40)
+e2.config(show="*")
+Button(root, text="Login", command=ok ,height= 3, width= 13).place(x=10, y=100)
+root.mainloop()
